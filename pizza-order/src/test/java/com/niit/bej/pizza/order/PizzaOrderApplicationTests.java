aee5b63ebcc37ced380668de12c26ba69d649844
Nihal <nihalmujawar28413@gmail.com>
2024-01-13 00:36:37 +0530

    initial commit

---
+++ pizza-order/src/test/java/com/niit/bej/pizza/order/PizzaOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niit.bej.pizza.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
