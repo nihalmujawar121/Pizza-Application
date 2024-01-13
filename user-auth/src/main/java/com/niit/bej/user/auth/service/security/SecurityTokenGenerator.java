48b82985405afe9e14a0470ef0fc0ca9ab66a678
Nihal <nihalmujawar28413@gmail.com>
2024-01-13 17:18:54 +0530

    added generateToken() abstract method to the SecurityTokenGenerator interface

--- user-auth/src/main/java/com/niit/bej/user/auth/service/security/SecurityTokenGenerator.java
+++ user-auth/src/main/java/com/niit/bej/user/auth/service/security/SecurityTokenGenerator.java
@@ -1,4 +1,9 @@
 package com.niit.bej.user.auth.service.security;
 
+import com.niit.bej.user.auth.model.User;
+
+import java.util.Map;
+
 public interface SecurityTokenGenerator {
+    Map<String,String> generateToken(User user);
 }
