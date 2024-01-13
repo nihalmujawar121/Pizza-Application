2831e8cdf052eba1783f6f2b7b0725fabd74496d
Nihal <nihalmujawar28413@gmail.com>
2024-01-13 18:40:46 +0530

    added registerUser() method and completed the method

--- user-auth/src/main/java/com/niit/bej/user/auth/controller/UserController.java
+++ user-auth/src/main/java/com/niit/bej/user/auth/controller/UserController.java
@@ -1,8 +1,14 @@
 package com.niit.bej.user.auth.controller;
 
+import com.niit.bej.user.auth.exception.UserAlreadyRegisteredException;
+import com.niit.bej.user.auth.model.User;
 import com.niit.bej.user.auth.service.UserService;
 import com.niit.bej.user.auth.service.security.SecurityTokenGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,12 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/home")
 public class UserController {
 
-    private UserService userService;
-    private SecurityTokenGenerator securityTokenGenerator;
+    private final UserService userService;
+    private final SecurityTokenGenerator securityTokenGenerator;
 
     @Autowired
     public UserController(UserService userService, SecurityTokenGenerator securityTokenGenerator) {
         this.userService = userService;
         this.securityTokenGenerator = securityTokenGenerator;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody User user) throws UserAlreadyRegisteredException {
+        User registerUser = userService.register(user);
+        return new ResponseEntity<>(registerUser, HttpStatus.CREATED);
     }
 }
