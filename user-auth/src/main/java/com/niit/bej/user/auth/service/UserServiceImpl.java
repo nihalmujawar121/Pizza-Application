d9d6c165a0a1e0bc01d392e046fb0fe1ed311e7d
Nihal <nihalmujawar28413@gmail.com>
2024-01-13 16:34:56 +0530

    implemented UserService interface methods to the UserServiceImpl class

---
+++ user-auth/src/main/java/com/niit/bej/user/auth/service/UserServiceImpl.java
@@ -0,0 +1,15 @@
+package com.niit.bej.user.auth.service;
+
+import com.niit.bej.user.auth.model.User;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public User register(User user) {
+        return null;
+    }
+
+    @Override
+    public User login(User user) {
+        return null;
+    }
+}
