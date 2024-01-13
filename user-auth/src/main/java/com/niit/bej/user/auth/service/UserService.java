a83932eea70e6eae0fa3ebf43576c25d3f3e9778
Nihal <nihalmujawar28413@gmail.com>
2024-01-13 16:33:28 +0530

    added abstract methods to the UserService interface

--- user-auth/src/main/java/com/niit/bej/user/auth/service/UserService.java
+++ user-auth/src/main/java/com/niit/bej/user/auth/service/UserService.java
@@ -1,4 +1,9 @@
 package com.niit.bej.user.auth.service;
 
+import com.niit.bej.user.auth.model.User;
+
 public interface UserService {
+    User register(User user);
+
+    User login(User user);
 }
