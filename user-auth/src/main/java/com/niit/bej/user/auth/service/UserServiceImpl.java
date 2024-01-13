6552d5b876a3b9e35923a7c6e0a333228422ac85
Nihal <nihalmujawar28413@gmail.com>
2024-01-13 16:38:21 +0530

    injected the dependency of UserRepository interface

--- user-auth/src/main/java/com/niit/bej/user/auth/service/UserServiceImpl.java
+++ user-auth/src/main/java/com/niit/bej/user/auth/service/UserServiceImpl.java
@@ -1,8 +1,17 @@
 package com.niit.bej.user.auth.service;
 
 import com.niit.bej.user.auth.model.User;
+import com.niit.bej.user.auth.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public User register(User user) {
         return null;
