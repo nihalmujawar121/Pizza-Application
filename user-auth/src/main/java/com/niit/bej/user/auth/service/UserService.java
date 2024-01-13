ead37f96bd593a882e0983d86d44dab67b7ff7ce
Nihal <nihalmujawar28413@gmail.com>
2024-01-13 17:15:25 +0530

    added exception to the login() method signature

--- user-auth/src/main/java/com/niit/bej/user/auth/service/UserService.java
+++ user-auth/src/main/java/com/niit/bej/user/auth/service/UserService.java
@@ -1,10 +1,12 @@
 package com.niit.bej.user.auth.service;
 
+import com.niit.bej.user.auth.exception.InvalidCredentialsException;
 import com.niit.bej.user.auth.exception.UserAlreadyRegisteredException;
+import com.niit.bej.user.auth.exception.UserNotFoundException;
 import com.niit.bej.user.auth.model.User;
 
 public interface UserService {
     User register(User user) throws UserAlreadyRegisteredException;
 
-    User login(User user);
+    User login(User user) throws UserNotFoundException, InvalidCredentialsException;
 }
