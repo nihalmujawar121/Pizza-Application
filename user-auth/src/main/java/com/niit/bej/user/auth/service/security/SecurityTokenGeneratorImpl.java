edb00cb163780c56112d081fb20e96afcf221dec
Nihal <nihalmujawar28413@gmail.com>
2024-01-13 17:42:57 +0530

    completed the generateToken() method

--- user-auth/src/main/java/com/niit/bej/user/auth/service/security/SecurityTokenGeneratorImpl.java
+++ user-auth/src/main/java/com/niit/bej/user/auth/service/security/SecurityTokenGeneratorImpl.java
@@ -1,12 +1,31 @@
 package com.niit.bej.user.auth.service.security;
 
 import com.niit.bej.user.auth.model.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Service;
 
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
+@Service
 public class SecurityTokenGeneratorImpl implements SecurityTokenGenerator {
     @Override
     public Map<String, String> generateToken(User user) {
-        return null;
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("userName", user.getUserName());
+        String token = Jwts.builder()
+                .setIssuedAt(new Date())
+                .setIssuer("UserAuth")
+                .setClaims(claims)
+                .setSubject(user.getUserName())
+                .signWith(SignatureAlgorithm.HS256, "password")
+                .compact();
+        return Map.of(
+                "token", token,
+                "message", "Login Successfully"
+        );
     }
 }
+
