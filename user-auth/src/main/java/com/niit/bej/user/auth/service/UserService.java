212fe394872fde7dc23bf8d669d6434e2c25e096
Nihal <nihalmujawar28413@gmail.com>
2024-01-13 16:50:15 +0530

    completed register() method, added exception to the method signature

--- user-auth/src/main/java/com/niit/bej/user/auth/service/UserService.java
+++ user-auth/src/main/java/com/niit/bej/user/auth/service/UserService.java
@@ -1,9 +1,10 @@
 package com.niit.bej.user.auth.service;
 
+import com.niit.bej.user.auth.exception.CustomerAlreadyRegisteredException;
 import com.niit.bej.user.auth.model.User;
 
 public interface UserService {
-    User register(User user);
+    User register(User user) throws CustomerAlreadyRegisteredException;
 
     User login(User user);
 }
--- user-auth/src/main/java/com/niit/bej/user/auth/service/UserServiceImpl.java
+++ user-auth/src/main/java/com/niit/bej/user/auth/service/UserServiceImpl.java
@@ -1,20 +1,28 @@
 package com.niit.bej.user.auth.service;
 
+import com.niit.bej.user.auth.exception.CustomerAlreadyRegisteredException;
 import com.niit.bej.user.auth.model.User;
 import com.niit.bej.user.auth.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Optional;
+
 public class UserServiceImpl implements UserService {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
     @Autowired
     public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     @Override
-    public User register(User user) {
-        return null;
+    public User register(User user) throws CustomerAlreadyRegisteredException {
+        Optional<User> optionalUser = this.userRepository.findById(user.getCustomerId());
+        if (optionalUser.isPresent()) {
+            throw new CustomerAlreadyRegisteredException("Customer name is taken, please choose another customer name");
+        }
+        return userRepository.save(user);
     }
 
     @Override
