b1bcbee3a6a7f4386bc9ef4f223fc86af4dac271
Nihal <nihalmujawar28413@gmail.com>
2024-01-13 14:16:44 +0530

    extended JpaRepository for the UserRepository

--- user-auth/src/main/java/com/niit/bej/user/auth/repository/UserRepository.java
+++ user-auth/src/main/java/com/niit/bej/user/auth/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.niit.bej.user.auth.repository;
 
+import com.niit.bej.user.auth.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User,Integer> {
 }
