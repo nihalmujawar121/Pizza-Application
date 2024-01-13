5e267ecb6d81d254c21ae168ee3959766e3b8b6f
Nihal <nihalmujawar28413@gmail.com>
2024-01-13 17:15:39 +0530

    completed the login() method

--- user-auth/src/main/java/com/niit/bej/user/auth/service/UserServiceImpl.java
+++ user-auth/src/main/java/com/niit/bej/user/auth/service/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.niit.bej.user.auth.service;
 
+import com.niit.bej.user.auth.exception.InvalidCredentialsException;
 import com.niit.bej.user.auth.exception.UserAlreadyRegisteredException;
+import com.niit.bej.user.auth.exception.UserNotFoundException;
 import com.niit.bej.user.auth.model.User;
 import com.niit.bej.user.auth.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,9 +28,17 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public User login(User user) {
-
-        return null;
+    public User login(User user) throws UserNotFoundException, InvalidCredentialsException {
+        Optional<User> optionalUser = this.userRepository.findById(user.getUserId());
+        if (optionalUser.isEmpty()) {
+            throw new UserNotFoundException("User not found !");
+        }
+        User userFromDatabase = optionalUser.get();
+        if (userFromDatabase.getPassword().equals(user.getPassword())){
+            return userFromDatabase;
+        }
+       throw new InvalidCredentialsException("Please check your username and password!");
     }
 }
+
 
