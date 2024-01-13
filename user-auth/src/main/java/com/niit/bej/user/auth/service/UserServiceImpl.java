7ac2dad08dbeb46c171b1b3b94d3cce4973b0118
Nihal <nihalmujawar28413@gmail.com>
2024-01-13 17:24:21 +0530

    added service annotation to the UserServiceImpl class

--- user-auth/src/main/java/com/niit/bej/user/auth/service/UserServiceImpl.java
+++ user-auth/src/main/java/com/niit/bej/user/auth/service/UserServiceImpl.java
@@ -6,9 +6,11 @@ import com.niit.bej.user.auth.exception.UserNotFoundException;
 import com.niit.bej.user.auth.model.User;
 import com.niit.bej.user.auth.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
