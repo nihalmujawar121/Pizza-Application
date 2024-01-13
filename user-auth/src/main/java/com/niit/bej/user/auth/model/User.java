cf2ec2ca3edd640e0c6b9b2c9e9d58f1abfa34ad
Nihal <nihalmujawar28413@gmail.com>
2024-01-13 14:14:13 +0530

    added fields for the User class

--- user-auth/src/main/java/com/niit/bej/user/auth/model/User.java
+++ user-auth/src/main/java/com/niit/bej/user/auth/model/User.java
@@ -1,4 +1,21 @@
 package com.niit.bej.user.auth.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class User {
+    @Id
+    private int customerId;
+    private String customerName;
+    private String password;
+    private String mobileNumber;
+    private String address;
+
 }
