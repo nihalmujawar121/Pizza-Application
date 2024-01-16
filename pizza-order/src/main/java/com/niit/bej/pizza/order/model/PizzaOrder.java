411a36409adaa6dda5380032bdfd5d815c3b7d77
Nihal <nihalmujawar28413@gmail.com>
2024-01-16 22:39:11 +0530

    added the lombok annotations

--- pizza-order/src/main/java/com/niit/bej/pizza/order/model/PizzaOrder.java
+++ pizza-order/src/main/java/com/niit/bej/pizza/order/model/PizzaOrder.java
@@ -1,5 +1,12 @@
 package com.niit.bej.pizza.order.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PizzaOrder {
     private int pizzaId;
     private String varietyOfPizza;
