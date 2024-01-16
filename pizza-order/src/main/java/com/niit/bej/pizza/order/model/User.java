cd7f007665c4d9e5591f2647fd8f1226569b6bc5
Nihal <nihalmujawar28413@gmail.com>
2024-01-16 23:30:02 +0530

    added fields to the User class

--- pizza-order/src/main/java/com/niit/bej/pizza/order/model/User.java
+++ pizza-order/src/main/java/com/niit/bej/pizza/order/model/User.java
@@ -1,4 +1,20 @@
 package com.niit.bej.pizza.order.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document
 public class User {
+    @MongoId
+    private String userEmailId;
+    private String userName;
+    private String password;
+    private String mobileNumber;
+    private String address;
 }
