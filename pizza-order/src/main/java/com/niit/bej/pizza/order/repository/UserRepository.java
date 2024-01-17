68237d2e15fda5be64f4dab77936e094e4f81c39
Nihal <nihalmujawar28413@gmail.com>
2024-01-17 20:25:07 +0530

    added the findUserByUsername() method

--- pizza-order/src/main/java/com/niit/bej/pizza/order/repository/UserRepository.java
+++ pizza-order/src/main/java/com/niit/bej/pizza/order/repository/UserRepository.java
@@ -5,6 +5,9 @@ import com.niit.bej.pizza.order.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends MongoRepository<User, PizzaOrder> {
+    Optional<User> findUserByUserName(String userName);
 }
