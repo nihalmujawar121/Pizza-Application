7d0992c2f4a96a68ddf70f575e27b73de799b324
Nihal <nihalmujawar28413@gmail.com>
2024-01-17 21:07:15 +0530

    implemented the UserService interface

---
+++ pizza-order/src/main/java/com/niit/bej/pizza/order/service/UserServiceImpl.java
@@ -0,0 +1,38 @@
+package com.niit.bej.pizza.order.service;
+
+import com.niit.bej.pizza.order.model.PizzaOrder;
+import com.niit.bej.pizza.order.model.User;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public User registerUser(User user) {
+        return null;
+    }
+
+    @Override
+    public User findUserByUsername(String username) {
+        return null;
+    }
+
+    @Override
+    public PizzaOrder addPizza(String username, PizzaOrder pizzaOrder) {
+        return null;
+    }
+
+    @Override
+    public PizzaOrder deletePizzaOrder(String username, String pizzaName) {
+        return null;
+    }
+
+    @Override
+    public List<PizzaOrder> viewCatalogue(String username) {
+        return null;
+    }
+
+    @Override
+    public List<PizzaOrder> getCarts(String username) {
+        return null;
+    }
+}
