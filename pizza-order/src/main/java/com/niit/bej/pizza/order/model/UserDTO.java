d926a70f47f5f85ca46ee08ae8bb05e496389abd
Nihal <nihalmujawar28413@gmail.com>
2024-01-17 17:26:56 +0530

    added the fields to the UserDTO class

--- pizza-order/src/main/java/com/niit/bej/pizza/order/model/UserDTO.java
+++ pizza-order/src/main/java/com/niit/bej/pizza/order/model/UserDTO.java
@@ -1,5 +1,9 @@
 package com.niit.bej.pizza.order.model;
 
 public class UserDTO {
-
+    private String userEmailId;
+    private String userName;
+    private String password;
+    private String mobileNumber;
+    private String address;
 }
