37ae075e443612a88c2d63de98e83f0baf575db3
Nihal <nihalmujawar28413@gmail.com>
2024-01-17 20:23:09 +0530

    renamed the UserRepository interface

--- pizza-order/src/main/java/com/niit/bej/pizza/order/repository/UserRepository.java
+++ pizza-order/src/main/java/com/niit/bej/pizza/order/repository/UserRepository.java
@@ -1,9 +1,10 @@
 package com.niit.bej.pizza.order.repository;
 
+import com.niit.bej.pizza.order.model.PizzaOrder;
 import com.niit.bej.pizza.order.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PizzaOrderRepository extends MongoRepository<User, Integer> {
+public interface UserRepository extends MongoRepository<User, PizzaOrder> {
 }
