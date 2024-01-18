3f8edd2071190ec7e2c4a2b0a2d51c15594d4b37
Nihal <nihalmujawar28413@gmail.com>
2024-01-18 21:06:43 +0530

    added saveUser() method and completed the method

--- pizza-order/src/main/java/com/niit/bej/pizza/order/controller/UserController.java
+++ pizza-order/src/main/java/com/niit/bej/pizza/order/controller/UserController.java
@@ -1,9 +1,14 @@
 package com.niit.bej.pizza.order.controller;
 
+import com.niit.bej.pizza.order.exception.UserAlreadyCreatedException;
+import com.niit.bej.pizza.order.model.User;
 import com.niit.bej.pizza.order.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,4 +17,14 @@ public class UserController {
 
     @Autowired
     UserService userService;
+
+    @PostMapping("/saveDetails")
+    public ResponseEntity<?> saveUser(@RequestBody User user) throws UserAlreadyCreatedException {
+        User details = userService.createUser(user);
+        if (details != null) {
+            return new ResponseEntity<>(details, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Error Occurred", HttpStatus.NOT_FOUND);
+        }
+    }
 }
