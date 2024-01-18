63701dc8096c9533563131f8d3b23416020eac18
Nihal <nihalmujawar28413@gmail.com>
2024-01-18 18:20:28 +0530

    added FeignClient annotation to the UserProxy interface

--- pizza-order/src/main/java/com/niit/bej/pizza/order/UserProxy/UserProxy.java
+++ pizza-order/src/main/java/com/niit/bej/pizza/order/UserProxy/UserProxy.java
@@ -1,4 +1,7 @@
 package com.niit.bej.pizza.order.UserProxy;
 
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient(name = "user-auth", url="https://localhost:8081")
 public interface UserProxy {
 }
