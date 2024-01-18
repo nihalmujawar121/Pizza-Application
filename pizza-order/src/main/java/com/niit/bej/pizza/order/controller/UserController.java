8f060ed5bbe396fccc85de5ace659480e2af8a4f
Nihal <nihalmujawar28413@gmail.com>
2024-01-18 20:49:48 +0530

    added RestController and RequestMapping annotations to the UserController class

--- pizza-order/src/main/java/com/niit/bej/pizza/order/controller/UserController.java
+++ pizza-order/src/main/java/com/niit/bej/pizza/order/controller/UserController.java
@@ -1,4 +1,12 @@
 package com.niit.bej.pizza.order.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v2")
 public class UserController {
+
+
 }
