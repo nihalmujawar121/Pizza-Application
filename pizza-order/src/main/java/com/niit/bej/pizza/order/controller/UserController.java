280565469331ff0e549f9158bd7c49f480b852da
Nihal <nihalmujawar28413@gmail.com>
2024-01-18 21:02:38 +0530

    injected the dependency of UserService interface

--- pizza-order/src/main/java/com/niit/bej/pizza/order/controller/UserController.java
+++ pizza-order/src/main/java/com/niit/bej/pizza/order/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.niit.bej.pizza.order.controller;
 
+import com.niit.bej.pizza.order.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,5 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v2")
 public class UserController {
 
-
+    @Autowired
+    UserService userService;
 }
