7ebd7e5f2591b8d380ac66ff4e94c7766e78baf4
Nihal <nihalmujawar28413@gmail.com>
2024-01-18 18:23:55 +0530

    added abstract method to the UserProxy interface

--- pizza-order/src/main/java/com/niit/bej/pizza/order/UserProxy/UserProxy.java
+++ pizza-order/src/main/java/com/niit/bej/pizza/order/UserProxy/UserProxy.java
@@ -1,7 +1,15 @@
 package com.niit.bej.pizza.order.UserProxy;
 
+import com.niit.bej.pizza.order.model.User;
+import com.niit.bej.pizza.order.model.UserDTO;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "user-auth", url="https://localhost:8081")
+@FeignClient(name = "user-auth", url = "https://localhost:8081")
 public interface UserProxy {
+
+    @PostMapping("api/v1/register")
+    ResponseEntity<User> registerUser(@RequestBody UserDTO user);
 }
