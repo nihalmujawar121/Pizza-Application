953b08874ef35e5ddd3c2fba83cada3720dd9614
Nihal <nihalmujawar28413@gmail.com>
2024-01-23 19:27:38 +0530

    implemented the UserService interface to UserServiceImpl class

---
+++ pizza/src/main/java/com/niit/bej/pizza/service/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package com.niit.bej.pizza.service;
+
+import com.niit.bej.pizza.model.User;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public User createUser(User user) {
+        return null;
+    }
+}
