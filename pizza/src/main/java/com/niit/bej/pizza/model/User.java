651ef3fe955a740ebf9f9713c3d0bc84e770bf51
Nihal <nihalmujawar28413@gmail.com>
2024-01-23 19:09:18 +0530

    added necessary annotations to the User class added dependency of mongodb

--- pizza/src/main/java/com/niit/bej/pizza/model/User.java
+++ pizza/src/main/java/com/niit/bej/pizza/model/User.java
@@ -1,9 +1,20 @@
 package com.niit.bej.pizza.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
 import java.util.List;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document
 public class User {
     List<PizzaOrder> cart;
+    @MongoId
     private String userEmailId;
     private String username;
     private String password;
