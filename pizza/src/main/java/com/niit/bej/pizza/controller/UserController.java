fdc47384bca0db05fa23cff638b3fb6b24d72874
Nihal <nihalmujawar28413@gmail.com>
2024-01-23 19:48:58 +0530

    added createUser() method and completed the method

--- pizza/src/main/java/com/niit/bej/pizza/controller/UserController.java
+++ pizza/src/main/java/com/niit/bej/pizza/controller/UserController.java
@@ -1,7 +1,13 @@
 package com.niit.bej.pizza.controller;
 
+import com.niit.bej.pizza.exception.UserAlreadyCreatedException;
+import com.niit.bej.pizza.model.User;
 import com.niit.bej.pizza.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +17,14 @@ public class UserController {
 
     @Autowired
     UserService userService;
+
+    @PostMapping("/create")
+    ResponseEntity<User> createUser(@RequestBody User user) {
+        try {
+            User newUser = userService.createUser(user);
+            return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+        } catch (UserAlreadyCreatedException exception) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+    }
 }
