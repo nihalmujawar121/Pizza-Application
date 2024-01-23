88ca992819e6466f0952078fb3e6523151a27614
Nihal <nihalmujawar28413@gmail.com>
2024-01-23 19:28:20 +0530

    injected the dependency of UserRepository interface

--- pizza/src/main/java/com/niit/bej/pizza/service/UserServiceImpl.java
+++ pizza/src/main/java/com/niit/bej/pizza/service/UserServiceImpl.java
@@ -1,8 +1,13 @@
 package com.niit.bej.pizza.service;
 
 import com.niit.bej.pizza.model.User;
+import com.niit.bej.pizza.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
     @Override
     public User createUser(User user) {
         return null;
