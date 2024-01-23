23c0e74fe05819e67ebdd2c4a9e9bb7dd68668a1
Nihal <nihalmujawar28413@gmail.com>
2024-01-23 19:13:54 +0530

    added Repository annotation to the UserRepository interface

--- pizza/src/main/java/com/niit/bej/pizza/repository/UserRepository.java
+++ pizza/src/main/java/com/niit/bej/pizza/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.niit.bej.pizza.repository;
 
-public interface UserRepository {
+import com.niit.bej.pizza.model.PizzaOrder;
+import com.niit.bej.pizza.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, PizzaOrder> {
 }
