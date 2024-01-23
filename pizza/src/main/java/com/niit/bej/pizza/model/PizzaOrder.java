03c49528fc42c100f1c022bfe2e5999800d1b2a6
Nihal <nihalmujawar28413@gmail.com>
2024-01-23 18:55:26 +0530

    added fields for the PizzaOrder class

--- pizza/src/main/java/com/niit/bej/pizza/model/PizzaOrder.java
+++ pizza/src/main/java/com/niit/bej/pizza/model/PizzaOrder.java
@@ -1,4 +1,8 @@
 package com.niit.bej.pizza.model;
 
 public class PizzaOrder {
+    private int pizzaId;
+    private String varietyOfPizza;
+    private int priceOfPizza;
+    private String sizeOfPizza;
 }
