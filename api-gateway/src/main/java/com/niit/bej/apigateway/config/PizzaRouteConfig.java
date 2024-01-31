1bab185352b753aad092f9940ef4f9da260f8b55
Nihal <nihalmujawar28413@gmail.com>
2024-01-31 20:39:41 +0530

    added Bean and LoadBalanced annotations

--- api-gateway/src/main/java/com/niit/bej/apigateway/config/PizzaRouteConfig.java
+++ api-gateway/src/main/java/com/niit/bej/apigateway/config/PizzaRouteConfig.java
@@ -1,11 +1,15 @@
 package com.niit.bej.apigateway.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class PizzaRouteConfig {
+    @Bean
+    @LoadBalanced
     public RouteLocator locatePizzaMicroserviceRoute(RouteLocatorBuilder routeLocatorBuilder){
         return routeLocatorBuilder
                 .routes()
