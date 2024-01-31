6444c7fea24e40e7efcc15eef56e876ea5c20c6c
Nihal <nihalmujawar28413@gmail.com>
2024-01-31 20:37:50 +0530

    added locatePizzaMicroserviceRoute() method and completed the method

--- api-gateway/src/main/java/com/niit/bej/apigateway/config/PizzaRouteConfig.java
+++ api-gateway/src/main/java/com/niit/bej/apigateway/config/PizzaRouteConfig.java
@@ -1,4 +1,16 @@
 package com.niit.bej.apigateway.config;
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class PizzaRouteConfig {
+    public RouteLocator locatePizzaMicroserviceRoute(RouteLocatorBuilder routeLocatorBuilder){
+        return routeLocatorBuilder
+                .routes()
+                .route(routePredicate -> routePredicate
+                        .path("/api/v2/pizza/**")
+                        .uri("lb://PizzaApplication")).build();
+    }
 }
