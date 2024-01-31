d89d3f769940ea112361b867c3935431a238f4fe
Nihal <nihalmujawar28413@gmail.com>
2024-01-31 20:30:20 +0530

    added locateUserAuthMicroserviceRoute() method

--- api-gateway/src/main/java/com/niit/bej/apigateway/config/UserAuthRouteConfig.java
+++ api-gateway/src/main/java/com/niit/bej/apigateway/config/UserAuthRouteConfig.java
@@ -1,4 +1,22 @@
 package com.niit.bej.apigateway.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class UserAuthRouteConfig {
+
+    @Bean
+    @LoadBalanced
+    public RouteLocator locateUserAuthMicroserviceRoute(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder
+                .routes()
+                .route(routePredicate -> routePredicate
+                        .path("/home/**")
+                        .uri("lb://UserAuth"))
+                .build();
+    }
 }
